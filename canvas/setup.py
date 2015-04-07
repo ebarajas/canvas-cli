99124f6fb3dd3b1e092d057f44b4ed0ce90706fb
Ernesto Barajas <ernestobarajas@utexas.edu>
2015-04-07 01:11:49 -0500

    added new dependencies to setup.py

--- canvas/setup.py
+++ canvas/setup.py
@@ -6,6 +6,12 @@ setup(
     py_modules=['canvas'],
     install_requires=[
         'Click',
+        'requests',
+        'urllib3', 
+        'pyopenssl',
+        'ndg-httpsclient',
+        'pyasn1',
+        'tabulate'
     ],
     entry_points='''
         [console_scripts]
